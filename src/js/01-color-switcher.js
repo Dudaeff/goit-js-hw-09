176f5ba59786792e3dd6392f993243c38ca358f9
Roma <r.dutkevich@gmail.com>
2022-12-22 00:35:18 +0200

    done first task and half second

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+let intervalId = null;
 
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+    stopBtn.removeAttribute('disabled');
+    startBtn.setAttribute('disabled', 'disabled');
+    changeBgColorByInterval();
+};
+
+function onStopBtnClick() {
+    startBtn.removeAttribute('disabled');
+    stopBtn.setAttribute('disabled', 'disabled');
+    clearInterval(intervalId);
+};
+
+function changeBgColorByInterval() {
+    return intervalId = setInterval(() => {
+        body.style.backgroundColor =  getRandomHexColor();
+    }, 1000)
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
